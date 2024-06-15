a624df09906bfdf1ce88a352fc46840bc4606825
Pascal <pascale.koop@live.com>
2024-06-15 17:55:42 +0200

    change event

--- components/TheInputField.vue
+++ components/TheInputField.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 	import { useTodosStore } from '~/store/todos';
 	const todoStore = useTodosStore();
+	const { addTodo } = todoStore;
 	const toDoInput = ref<string>('');
 </script>
 
@@ -8,7 +9,7 @@
 
 <template>
 	<input
-		@keydown.enter="todoStore.addTodo(toDoInput)"
+		@keydown.enter="addTodo(toDoInput)"
 		v-model="toDoInput"
 		type="text"
 		placeholder="Type something here" />
